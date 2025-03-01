cf132ed9678974ac3cebe6b30a252cf7f7a89432
Daire <bohan.daire@gmail.com>
2025-03-01 13:10:11 +0000

    fix: update import path for SharedLogic in test.ts

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,7 @@
 
 // Ensure you import h5.js before SharedLogic.js
 require(`../dist/h5.js`);
-require(`../dist/MySharedLogic.js`); // any static functions labelled "Main" will run on require
+require(`../dist/SharedLogic.js`); // any static functions labelled "Main" will run on require
 
 // Running static methods directly from the namespace
 SharedLogic.TestProgram.PrintTest();
